075d0d351431aa7192d7b996becda288a589f802
Davyd Svidnitskyi <professional.github@davyd-svidnitskyi.me>
2026-04-20 16:31:27 -0600

    refactor: Remove unnecessary _get_headers() method, update docstrings

--- core/services/rating/get_rates.py
+++ core/services/rating/get_rates.py
@@ -5,12 +5,13 @@ from typing import Optional, Literal
 import requests
 from requests import Response
 
-from .types import Destination, DomesticDestination, Option, ParcelCharacteristics, Rates, Rate
+from .types import Destination, DomesticDestination, Option, ParcelCharacteristics, Rate
 from ...utils.construct_xml_element import ConstructXMLElement
 from ...utils.error_handler import error_handler
 from ...utils.response_to_object.rate_to_object import RateToObject
 
 construct = ConstructXMLElement()
+
 
 class GetRates:
 
@@ -19,16 +20,6 @@ class GetRates:
         self.endpoint = endpoint
         self.customer_number = customer_number
         self.contract_id = contract_id
-
-    def _get_headers(self) -> dict:
-        headers = self.headers.copy()
-        headers.update(
-            {
-                "Accept": "application/vnd.cpc.ship.rate-v4+xml",
-                "Content-Type": "application/vnd.cpc.ship.rate-v4+xml"
-            }
-        )
-        return headers
 
     @error_handler
     def get_rates(
@@ -43,7 +34,7 @@ class GetRates:
         unpackaged: bool = False,
         mailing_tube: bool = False,
         oversized: bool = False,
-        services: list[Literal[
+        services: Optional[list[Literal[
             "DOM.RP",
             "DOM.EP",
             "DOM.XP",
@@ -61,46 +52,65 @@ class GetRates:
             "INT.SP.AIR",
             "INT.SP.SURF",
             "INT.TP"
-        ]] = None
+        ]]] = None
     ) -> Response:
         """
         Function to get rates for a shipping based on the provided arguments.
 
         Parameters
         ----------
-        origin_postal_code
+        origin_postal_code : str
             Postal Code from which the parcel will be sent.
             Format ANANAN (only accepted with uppercase)
-        destination
+        destination : Destination
             Defines the destination of the parcel.
-        promo_code
+        promo_code : Optional[str], optional
             If you have a promotional discount code, enter it here. The discount amount will be returned in the response under the adjustment structure.
-        quote_type
+        quote_type : Optional[Literal["commercial", "counter"]], default "commercial"
             Either commercial or counter.
 
             - "commercial" will return the discounted price for the commercial customer or Solutions for Small Business member.
             - "counter" will return the regular price paid by consumers.
             Defaults to "commercial" if not specified.
-        expected_mailing_date
+        expected_mailing_date : Optional[datetime], optional
             The expected mailing date for the parcel.
 
             This date is used in calculations of the expected delivery date, however all rate quotes are based on the current system date.
-        options
+        options : Optional[list[Option]], optional
             Structure containing the list of options desired for the shipment.
-        parcel_characteristics
+        parcel_characteristics : Optional[ParcelCharacteristics], optional
             Details of the parcel such as weight and dimensions.
-        unpackaged
+        unpackaged : bool, default False
             Indicates that the parcel will be unpackaged (e.g. tires)
-        mailing_tube
+        mailing_tube : bool, default False
             Indicates that the object will be shipped in a mailing tube
-        oversized
+        oversized : bool, default False
             Indicates that the object has oversized dimensions. Automatically set correctly if dimensions are provided.
-        services
+        services : Optional[list[Literal[
+            "DOM.RP",
+            "DOM.EP",
+            "DOM.XP",
+            "DOM.XP.CERT",
+            "DOM.PC",
+            "DOM.LIB",
+            "USA.EP",
+            "USA.SP.AIR",
+            "USA.TP",
+            "USA.TP.LVM",
+            "USA.XP",
+            "INT.XP",
+            "INT.IP.AIR",
+            "INT.IP.SURF",
+            "INT.SP.AIR",
+            "INT.SP.SURF",
+            "INT.TP"
+        ]]], optional
             List of services to be used for the shipment.
 
         Returns
         -------
-        Response object
+        Response
+            Response object
 
         Examples
         --------
@@ -116,7 +126,7 @@ class GetRates:
         >>>    contract_id=contract_id
         >>> )
 
-        >>> response = py_canada_post.rates.rating(
+        >>> response = py_canada_post.rating.rates.get_rates(
         >>>    origin_postal_code="E4M8S3",
         >>>    destination=Destination(
         >>>        domestic=DomesticDestination(
@@ -167,14 +177,14 @@ class GetRates:
         response = requests.post(
             url=f"{self.endpoint}/rs/ship/price",
             data=ET.tostring(mailing_scenario, encoding='utf-8'),
-            headers=self._get_headers(),
+            headers=self.headers,
         )
 
         return response
 
     @staticmethod
     def get_rate_by_service(
-        rates: Rates,
+        rates: list[Rate],
         service_code: Literal[
             "DOM.RP",
             "DOM.EP",
@@ -195,7 +205,40 @@ class GetRates:
             "INT.TP"
         ]
     ) -> Optional[Rate]:
-        for rate in rates.rates:
+        """
+        Function to get a rate based on the service provided
+
+        Parameters
+        ----------
+        rates : list[Rate]
+            List of rates available
+        service_code : Literal[
+            "DOM.RP",
+            "DOM.EP",
+            "DOM.XP",
+            "DOM.XP.CERT",
+            "DOM.PC",
+            "DOM.LIB",
+            "USA.EP",
+            "USA.SP.AIR",
+            "USA.TP",
+            "USA.TP.LVM",
+            "USA.XP",
+            "INT.XP",
+            "INT.IP.AIR",
+            "INT.IP.SURF",
+            "INT.SP.AIR",
+            "INT.SP.SURF",
+            "INT.TP"
+        ]
+            Service code to search for
+
+        Returns
+        -------
+        Optional[Rate]
+            Rate or None
+        """
+        for rate in rates:
             if rate.service.code == service_code:
                 return rate
 
@@ -203,6 +246,18 @@ class GetRates:
 
 
     @staticmethod
-    def rate_to_object(response: Response):
-        return RateToObject(response=response).response_to_object()
+    def rate_to_object(response: Response) -> Optional[list[Rate]]:
+        """
+        Function to transform Response type object into the readable and manageable dataclass format
 
+        Parameters
+        ----------
+        response : Response
+            Response type object
+
+        Returns
+        -------
+        Optional[list[Rate]]
+            List of rate or None
+        """
+        return RateToObject(response=response).response_to_object()
