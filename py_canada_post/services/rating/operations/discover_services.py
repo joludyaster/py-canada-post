ebb7fd4f2a51b5d7b1fe8544db55d421f34c038e
Davyd Svidnitskyi <professional.github@davyd-svidnitskyi.me>
2026-05-10 18:22:44 -0600

    refactor: Change discover_services.py
    Changes:
    - Move from ../core/services/rating to ../py_canada_post/services/rating/operations
    - Change types

--- py_canada_post/services/rating/operations/discover_services.py
+++ py_canada_post/services/rating/operations/discover_services.py
@@ -1,22 +1,21 @@
-from typing import Optional, Any
-
 import requests
-from requests import Response
 
-from core.services.rating.types import Service
-from core.utils.error_handler import error_handler
-from core.utils.response_to_object.service_to_object import ServiceToObject
+from typing import Any
+from requests import Response
+from py_canada_post.services.rating.types import Service
+from py_canada_post.utils.error_handler import error_handler
+from py_canada_post.utils.response_to_object.serialization.service_to_object import ServiceToObject
 
 
 class DiscoverServices:
 
-    def __init__(self, headers: dict, endpoint: str, customer_number: int, contract_id: Optional[int] = None) -> None:
+    def __init__(self, headers: dict, endpoint: str, customer_number: int, contract_id: int = None) -> None:
         self.headers = headers
         self.endpoint = endpoint
         self.customer_number = customer_number
         self.contract_id = contract_id
 
-    def _construct_url(self, country_code: str, origin_postal_code: Optional[str] = None, destination_postal_code: Optional[str] = None) -> str:
+    def _construct_url(self, country_code: str, origin_postal_code: int = None, destination_postal_code: str = None) -> str:
         """
         Function to generate url to get the services based on the country code, origin postal code, destination postal code and contract number
 
@@ -24,9 +23,9 @@ class DiscoverServices:
         ----------
         country_code : str
             Country code in a 2-letter format (e.g. JP, CA, US)
-        origin_postal_code : Optional[str], optional
+        origin_postal_code : str, optional
             Origin postal code where the package will be sent from
-        destination_postal_code : Optional[str], optional
+        destination_postal_code : str, optional
             Destination postal code where the package will be delivered to
 
         Returns
@@ -48,7 +47,7 @@ class DiscoverServices:
         return endpoint
 
     @error_handler
-    def discover_services(self, country_code: str, origin_postal_code: Optional[str] = None, destination_postal_code: Optional[str] = None) -> Any:
+    def discover_services(self, country_code: str, origin_postal_code: str = None, destination_postal_code: str = None) -> Any:
         """
         Function to discover services based on the country code, origin postal code, destination postal code and contract number
 
@@ -56,9 +55,9 @@ class DiscoverServices:
         ----------
         country_code : str
             Country code in a 2-letter format (e.g. JP, CA, US)
-        origin_postal_code : Optional[str], optional
+        origin_postal_code : str, optional
             Origin postal code where the package will be sent from
-        destination_postal_code : Optional[str], optional
+        destination_postal_code : str, optional
             Destination postal code where the package will be delivered to
 
         Returns
@@ -66,11 +65,7 @@ class DiscoverServices:
         Any
             Response object
         """
-        url = self._construct_url(
-            country_code=country_code,
-            origin_postal_code=origin_postal_code,
-            destination_postal_code=destination_postal_code
-        )
+        url = self._construct_url(country_code, origin_postal_code, destination_postal_code)
         response = requests.get(
             url=url,
             headers=self.headers,
@@ -79,7 +74,7 @@ class DiscoverServices:
         return response
 
     @staticmethod
-    def service_to_object(response: Response) -> Optional[list[Service]]:
+    def service_to_object(response: Response) -> list[Service] | None:
         """
         Function to transform a response object into the readable and manageable dataclass format
 
@@ -90,7 +85,7 @@ class DiscoverServices:
 
         Returns
         -------
-        Optional[list[Service]]
+        list[Service] or None
             List of services or None
         """
-        return ServiceToObject(response=response).response_to_object()
+        return ServiceToObject(response).response_to_object()
