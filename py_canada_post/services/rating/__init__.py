a4df108e3fa3b6ad47ab69f0674ebc4fbaef27f0
Davyd Svidnitskyi <professional.github@davyd-svidnitskyi.me>
2026-05-10 18:17:30 -0600

    refactor: Move __init__.py files + add some imports and remove unnecessary logic

---
+++ py_canada_post/services/rating/__init__.py
@@ -0,0 +1,33 @@
+from py_canada_post.services.rating.types import (
+    Option,
+    DomesticDestination,
+    UnitedStatesDestination,
+    InternationalDestination,
+    Destination,
+    Dimensions,
+    ParcelCharacteristics,
+    RateAdjustment,
+    RateOption,
+    RateTaxDetails,
+    RateTax,
+    RateService,
+    Rate,
+    Service
+)
+
+__all__ = [
+    "Option",
+    "DomesticDestination",
+    "UnitedStatesDestination",
+    "InternationalDestination",
+    "Destination",
+    "Dimensions",
+    "ParcelCharacteristics",
+    "RateAdjustment",
+    "RateOption",
+    "RateTaxDetails",
+    "RateTax",
+    "RateService",
+    "Rate",
+    "Service"
+]
